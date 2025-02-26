0d00fbc39548bed835a7e13144d96635ac48e33b
jagasia <rjagadeeswaran@Yahoo.com>
2025-02-26 20:34:21 +0530

    second

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main(String []args){
 		System.out.println("Hello UST Wave 5 Batch 2...This program is pulled and run from git");
+		System.out.println("We have done few changes in this code now. After we push this to git. see what happens");
 	}
 }
